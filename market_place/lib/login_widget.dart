4751e1f910988b596ec8a6eec17f0682fdccf4eb
Jlisowskyy <Jlisowskyy@gmail.com>
2024-09-01 17:10:19 +0200

    Correctly working login

--- market_place/lib/login_widget.dart
+++ market_place/lib/login_widget.dart
@@ -1,9 +1,27 @@
+@JS()
+library my_lib; //Not avoid the library annotation
+
+import 'package:js/js.dart';
+
 import 'package:flutter/material.dart';
 import 'package:market_place/rounded_container.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'user_account.dart';
+import 'dart:js' as js;
+import 'dart:js_util' as jsu;
 
-class LoginWidget extends StatelessWidget {
+@JS()
+external connect();
+
+class LoginWidget extends StatefulWidget {
   const LoginWidget({Key? key}) : super(key: key);
+
+  @override
+  _LoginWidgetState createState() => _LoginWidgetState();
+}
+
+class _LoginWidgetState extends State<LoginWidget> {
+  String? _loginStatus;
 
   @override
   Widget build(BuildContext context) {
@@ -11,49 +29,96 @@ class LoginWidget extends StatelessWidget {
       final parentWidth = constraints.maxWidth;
       final parentHeight = constraints.maxHeight;
 
-      return _buildWidget(parentWidth, parentHeight);
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          _buildWidget(parentWidth, parentHeight),
+          if (_loginStatus == "FAIL")
+            const Padding(
+              padding: EdgeInsets.only(top: 16.0),
+              child: Text(
+                "Failed to login",
+                style: TextStyle(color: Colors.red, fontSize: 28),
+              ),
+            ),
+        ],
+
+      );
     });
   }
 
-  void _loginUser() {}
+  Future<void> _loginUser() async {
+    try {
+      var jsPromise = connect();
+      String result = await jsu.promiseToFuture<String>(jsPromise);
+
+      setState(() {
+        _loginStatus = result;
+      });
+    } catch (e) {
+      print('Error during login: $e');
+      setState(() {
+        _loginStatus = "FAIL";
+      });
+    }
+  }
 
   Widget _buildWidget(double width, double height) {
     return RoundedContainer(
-        width: 0.3 * width,
-        height: 0.4 * height,
-        borderColor: Colors.white,
-        child: Padding(
-            padding: const EdgeInsets.all(32),
-            child: Center(
-                child: Column(
-              children: [
-                Expanded(
-                    flex: 7,
-                    child: Center(
-                        child: Row(
-                      children: [
-                        Image.asset('assets/metamask.png'),
-                        const Text(
-                          "Use your Metamask wallet.",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 28,
-                              color: Colors.black),
-                        )
-                      ],
-                    ))),
-                Expanded(
-                    flex: 3,
-                    child: ElevatedButton(
+      width: 0.3 * width,
+      height: 0.4 * height,
+      borderColor: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Center(
+          child: Column(
+            children: [
+              Expanded(
+                flex: 7,
+                child: Center(
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Image.asset('assets/metamask.png'),
+                      const Text(
+                        "Use your Metamask wallet.",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 28,
+                          color: Colors.black,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 3,
+                child: _loginStatus == "SUCCESS"
+                    ? const Text(
+                        "Successfully logged in",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 38,
+                          color: Colors.green,
+                        ),
+                      )
+                    : ElevatedButton(
                         onPressed: _loginUser,
                         child: const Text(
                           "Login",
                           style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18,
-                              color: Colors.black),
-                        )))
-              ],
-            ))));
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
