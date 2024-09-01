187978057b338b06c67f978452955171b07c6175
Jlisowskyy <Jlisowskyy@gmail.com>
2024-09-01 20:41:51 +0200

    Selectable game info

--- market_place/lib/games/game_blank_widget.dart
+++ market_place/lib/games/game_blank_widget.dart
@@ -9,39 +9,46 @@ class BlankGameInfo {
 
 class GameBlankWidget extends StatelessWidget {
   final BlankGameInfo _info;
+  final VoidCallback? onClick;
 
-  const GameBlankWidget({super.key, required BlankGameInfo info})
-      : _info = info;
+  const GameBlankWidget({
+    super.key,
+    required BlankGameInfo info,
+    this.onClick,
+  }) : _info = info;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity, // Use all available horizontal space
-      child: Card(
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Image.asset(
-                _info.path,
-                width: 64,
-                height: 64,
-                fit: BoxFit.cover,
-              ),
-              const SizedBox(
-                height: 64, // Match the height of the image
-                child: VerticalDivider(
-                  thickness: 4,
-                  width: 32,
-                  color: Colors.black,
+      child: InkWell(
+        onTap: onClick,
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Image.asset(
+                  _info.path,
+                  width: 64,
+                  height: 64,
+                  fit: BoxFit.cover,
                 ),
-              ),
-              Text(
-                _info.name,
-                style: const TextStyle(fontSize: 18),
-              ),
-            ],
+                const SizedBox(
+                  height: 64, // Match the height of the image
+                  child: VerticalDivider(
+                    thickness: 4,
+                    width: 32,
+                    color: Colors.black,
+                  ),
+                ),
+                Text(
+                  _info.name,
+                  style: const TextStyle(fontSize: 18),
+                ),
+              ],
+            ),
           ),
         ),
       ),
