ba838d0afa20a27db35e989e3f528f6e69fa9c6e
Jakub Lisowski <Jlisowskyy@gmail.com>
2024-09-04 23:40:55 +0200

    Fixed Johny

--- market_place/lib/nft_mock1.dart
+++ market_place/lib/nft_mock1.dart
@@ -1,16 +1,63 @@
 import 'nft_token.dart';
 
 const List<NftToken> mock0 = [
-  NftToken(ContractId: "0", OwnerId: "0", Description: "0", Rarity: "common", Name: "shit", TokenId: "1"),
-  NftToken(ContractId: "0", OwnerId: "0", Description: "1", Rarity: "rare", Name: "silver", TokenId: "2"),
-  NftToken(ContractId: "0", OwnerId: "0", Description: "2", Rarity: "Legendary", Name: "Diamond", TokenId: "3"),
-  NftToken(ContractId: "0", OwnerId: "0", Description: "3", Rarity: "Mega Rare", Name: "gold", TokenId: "4")
+  NftToken(
+      ContractId: "0",
+      OwnerId: "0",
+      Description: "0",
+      Rarity: "common",
+      Name: "shit",
+      TokenId: "1"),
+  NftToken(
+      ContractId: "0",
+      OwnerId: "0",
+      Description: "1",
+      Rarity: "rare",
+      Name: "silver",
+      TokenId: "2"),
+  NftToken(
+      ContractId: "0",
+      OwnerId: "0",
+      Description: "2",
+      Rarity: "Legendary",
+      Name: "Diamond",
+      TokenId: "3"),
+  NftToken(
+      ContractId: "0",
+      OwnerId: "0",
+      Description: "3",
+      Rarity: "Mega Rare",
+      Name: "gold",
+      TokenId: "4")
 ];
 
 const List<NftToken> mock1 = [
-  NftToken(ContractId: "0", OwnerId: "1", Description: "0", Rarity: "common", Name: "shit", TokenId: "1"),
-  NftToken(ContractId: "0", OwnerId: "1", Description: "1", Rarity: "rare", Name: "silver", TokenId: "2"),
-  NftToken(ContractId: "0", OwnerId: "1", Description: "2", Rarity: "Legendary", Name: "Diamond", TokenId: "3"),
-  NftToken(ContractId: "0", OwnerId: "1", Description: "3", Rarity: "Mega Rare", Name: "gold", TokenId: "4")
+  NftToken(
+      ContractId: "0",
+      OwnerId: "0x0829a8908AC3D2a225FCF01be9f9178D0aaebA21",
+      Description: "0",
+      Rarity: "common",
+      Name: "shit",
+      TokenId: "1"),
+  NftToken(
+      ContractId: "0",
+      OwnerId: "0x0829a8908AC3D2a225FCF01be9f9178D0aaebA21",
+      Description: "1",
+      Rarity: "rare",
+      Name: "silver",
+      TokenId: "2"),
+  NftToken(
+      ContractId: "0",
+      OwnerId: "0x0829a8908AC3D2a225FCF01be9f9178D0aaebA21",
+      Description: "2",
+      Rarity: "Legendary",
+      Name: "Diamond",
+      TokenId: "3"),
+  NftToken(
+      ContractId: "0",
+      OwnerId: "0x0829a8908AC3D2a225FCF01be9f9178D0aaebA21",
+      Description: "3",
+      Rarity: "Mega Rare",
+      Name: "gold",
+      TokenId: "4")
 ];
-
