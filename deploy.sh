2bc3401cc3b6bae662de945167865d644aef6755
Łukasz Kryczka <lukasz.kryczka000@gmail.com>
2024-09-12 16:59:36 +0200

    Updated deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,17 @@
+# Setup gcloud
+echo "Listing projects"
+gcloud projects list
+echo "Specify the project ID for deployment"
+project_id=""
+read project_id
+if [ -z "$project_id" ]
+then
+    echo "Project ID is empty"
+    exit 1
+fi
+
+gcloud config set project $project_id
+
 # Prepare market_place for deployment
 echo "Preparing market_place for deployment"
 cd ./market_place/
@@ -23,15 +37,4 @@ cd ../
 
 # Deploy
 echo "Deploying to Google Cloud"
-
-echo "Listing projects"
-gcloud projects list
-echo "Specify the project ID for deployment"
-project_id=""
-read project_id
-if [ -z "$project_id" ]
-then
-    echo "Project ID is empty"
-    exit 1
-fi
 gcloud app deploy --project $project_id gcloud_dummy/app.yaml market_place/build/web/app.yaml snake_game/dist/app.yml dino_game_node/dist/app.yml
